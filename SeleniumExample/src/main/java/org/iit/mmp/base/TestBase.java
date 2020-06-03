091ce92279f92dd860b90d29284e14bd58a8d77c
NithyaGR <62567716+NithyaGR@users.noreply.github.com>
2020-06-03 18:32:58 -0400

    Update TestBase.java
    simulating student 2's day 1 completed and committed job.

--- SeleniumExample/src/main/java/org/iit/mmp/base/TestBase.java
+++ SeleniumExample/src/main/java/org/iit/mmp/base/TestBase.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 //adding the comments - successfully editing from gitbash
+//editting and going to commit and merge - simulating as a student 2's day 1 job
 public class TestBase {
 	
 	protected WebDriver driver;
